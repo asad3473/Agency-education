6a8706faeb0451a18cbe6706a9b8cd4cb54a65f2
malikumair687 <malikumairbwp786@gmail.com>
2025-07-01 13:19:51 +0500

    nothig

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -4,16 +4,17 @@ import { FaWhatsapp } from "react-icons/fa6";
 import { FaSquareFacebook } from "react-icons/fa6";
 import { FaTwitterSquare } from "react-icons/fa";
 import { FaSquareInstagram } from "react-icons/fa6";
+import { MdOutlineMenuBook } from "react-icons/md";
 
 export default function Navbar() {
   return (
-    <div>
+    <div className="bg-[#0A0851] text-white">
       <div className="w-[90vw] mx-auto">
         {/* <!-- ========== First Section ========== --> */}
-        <div className="p-5 border-b border-gray-400 flex justify-between ">
+        <div className="p-5 border-b gap-3 border-gray-400 flex flex-col justify-center items-center sm:flex-row sm:justify-between ">
           <div className="flex gap-4 items-center">
-            <FaWhatsapp className="text-2xl" />
-            <span>+9080808008088</span>
+            <FaWhatsapp className="text-2xl text-color2" />
+            <span className="text-lg font-semibold">+9080808008088</span>
           </div>
           <div className="flex gap-5 items-center">
             <div>
@@ -32,23 +33,49 @@ export default function Navbar() {
         {/* <!-- =========First Section ========== --> */}
 
         {/* <!-- ========== second  Section ========== --> */}
-        <div className="flex justify-between py-10 ">
-          <div className="text-3xl">
-            Logo
+        <div className="flex whitespace-nowrap justify-between items-center py-10 ">
+          <div className="text-3xl w-15 h-15 sm:w-25 sm:h-25 ">
+            <img className="w-full rounded-full cursor-pointer h-full object-cover bg-center" src="https://plus.unsplash.com/premium_photo-1669652639337-c513cc42ead6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D" alt="" />
           </div>
-          <div className="flex items-center">
-            <ul className="flex gap-4 mr-5">
-              <li>Home</li>
-              <li>Universites</li>
-              <li>Admission</li>
-              <li>e-Subagent</li>
-              <li>North Cyprus</li>
-              <li>Contact</li>
-              <li>Login</li>
+          <div className="text-4xl lg:hidden ">
+            <MdOutlineMenuBook  />
+
+          </div>
+      {/* <!-- ========== Mobile Side Bar Section ========== --> */}
+
+      <div>
+      <div className="flex flex-col  items-center">
+            <ul className="flex flex-col items-center gap-4 text-lg font-semibold gap-4 mr-5">
+              <li className="cursor-pointer">Home</li>
+              <li className="cursor-pointer">Universites</li>
+              <li className="cursor-pointer">Admission</li>
+              <li className="cursor-pointer">e-Subagent</li>
+              <li className="cursor-pointer">North Cyprus</li>
+              <li className="cursor-pointer">Contact</li>
+              <li className="cursor-pointer">Login</li>
             </ul>
-            <button className="px-3 py-2 text-lg  font-semibold">Apply Now</button>
+            <button className="px-3 py-2 text-lg bg-color1 cursor-pointer rounded-sm font-semibold">Apply Now</button>
           </div>
-          <div>Select Language</div>
+      </div>
+      
+      {/* <!-- =========Mobile Side Bar Section ========== --> */}
+      
+
+          <div className="hidden lg:block">
+            <div className="flex hidden items-center">
+            <ul className="flex text-lg font-semibold gap-4 mr-5">
+              <li className="cursor-pointer">Home</li>
+              <li className="cursor-pointer">Universites</li>
+              <li className="cursor-pointer">Admission</li>
+              <li className="cursor-pointer">e-Subagent</li>
+              <li className="cursor-pointer">North Cyprus</li>
+              <li className="cursor-pointer">Contact</li>
+              <li className="cursor-pointer">Login</li>
+            </ul>
+            <button className="px-3 py-2 text-lg bg-color1 cursor-pointer rounded-sm font-semibold">Apply Now</button>
+          </div>
+          </div>
+          <div className="text-lg hidden lg:block cursor-pointer font-semibold">Select Language</div>
 
         </div>
         {/* <!-- =========second  Section ========== --> */}
