c1d97010c3e0edc7c342e2b4ce4b9dff1a7cf029
asad3473 <asadsoomro0311@gmail.com>
2025-07-01 11:35:41 +0500

    agency-education

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+import './App.css'
+import Navbar from './Pages/Navbar'
+import Home from './Pages/Home'
+import Fotter from './Pages/Fotter'
+
+function MainLayout() {
+  return (
+    <div className=' min-h-screen overflow-auto'>
+      <Navbar />
+      <Outlet />
+      <Fotter />
+    </div>
+  )
+}
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/', element: <Home />
+      },
+      {
+        path: '/service', element: <h1>i'am from service</h1>
+      },
+      {
+        path: '/about', element: <h1>i'm from about</h1>
+      },
+    ]
+  }
+])
+function App() {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
---
+++ src/Pages/Fotter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Fotter() {
+  return (
+    <div>
+        <h1>foooter</h1>
+    </div>
+  )
+}
