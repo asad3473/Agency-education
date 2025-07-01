24f39a6c9a0427a0ab5f04dad768aca746f17eb6
malikumair687 <malikumairbwp786@gmail.com>
2025-07-01 15:34:17 +0500

    Navbar and Footer Done

--- src/Pages/Fotter.jsx
+++ src/Pages/Fotter.jsx
@@ -1,9 +1,104 @@
-import React from 'react'
+import React from "react";
+import { FaSquarePhoneFlip } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { Link } from "react-router-dom";
 
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaTwitterSquare } from "react-icons/fa";
+import { FaSquareInstagram } from "react-icons/fa6";
 export default function Fotter() {
   return (
-    <div>
-        <h1>foooter</h1>
+    <div className="bg-color1 pt-10 text-white">
+      <div className="w-[90vw] grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 sm:gap-15 mx-auto p-5">
+        <div>
+          <Link to="/"><h1 className="text-2xl mb-3">AgencyEducation</h1></Link>
+          <p className="mb-5">
+            A Wonderful Education and Tec Skills learning Platform
+          </p>
+          <Link to="/contact">
+            {" "}
+            <button className="px-3 py-2 btnbg text-lg bg-color1 cursor-pointer rounded-sm font-semibold">
+              Apply Now
+            </button>
+          </Link>
+        </div>
+        <div>
+          <h1 className="text-xl mb-3">EducationUnlimited</h1>
+          <ul className="flex flex-col gap-3">
+            <Link to="">
+              <li>Home </li>
+            </Link>
+            <Link to="">
+              <li>Universites </li>
+            </Link>
+            <Link to="">
+              <li>About North Cyprus </li>
+            </Link>
+            <Link to="">
+              <li>Gallery </li>
+            </Link>
+            <Link to="">
+              <li>FAQ </li>
+            </Link>
+          </ul>
+        </div>
+        <div>
+          <h1 className="text-xl mb-3">Others Links</h1>
+          <ul className="flex flex-col gap-3">
+            <Link to="">
+              Admission Process <li></li>
+            </Link>
+            <Link to="">
+              e-subagent <li></li>
+            </Link>
+            <Link to="">
+              Contact <li></li>
+            </Link>
+          </ul>
+        </div>
+        <div>
+          <h1 className="text-xl mb-3">Contact Us</h1>
+          <ul className="flex flex-col gap-3">
+            <Link to="">
+              <li className="flex items-center gap-3">
+                <FaSquarePhoneFlip />
+                +905338899948
+              </li>
+            </Link>
+            <Link to="">
+              <li className="flex items-center gap-3">
+                <MdEmail />
+                info@studygists.com
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <div className="flex flex-col sm:flex-row justify-center gap-5 sm:gap-10 py-5 items-center">
+        <div className=" flex justify-center  gap-5  text-center">
+          <Link to="/facebook">
+            <div>
+              <FaSquareFacebook className="text-3xl cursor-pointer" />
+            </div>
+          </Link>
+
+          <Link to="/twitter">
+            <div>
+              <FaTwitterSquare className="text-3xl cursor-pointer" />
+            </div>
+          </Link>
+
+          <Link to="/instagram">
+            <div>
+              <FaSquareInstagram className="text-3xl cursor-pointer" />
+            </div>
+          </Link>
+        </div>
+        <div>
+          <p>Copyright @AgencyEducation Limited.</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import { FaWhatsapp } from "react-icons/fa6";
 import { FaSquareFacebook } from "react-icons/fa6";
@@ -26,7 +27,9 @@ export default function Navbar() {
         {/* <!-- ========== First Section ========== --> */}
         <div className="p-5 border-b gap-3 border-gray-400 flex flex-col justify-center items-center sm:flex-row sm:justify-between ">
           <div className="flex gap-4 items-center">
-            <FaWhatsapp className="text-2xl text-color2" />
+            <Link to="/">
+              <FaWhatsapp className="text-2xl text-color2" />
+            </Link>
             <span className="text-lg font-semibold">+9080808008088</span>
           </div>
           <div className="flex gap-2 items-center">
@@ -48,15 +51,23 @@ export default function Navbar() {
             <div className="hidden lg:block">
               {" "}
               <div className="flex gap-5    lg:flex">
-                <div>
-                  <FaSquareFacebook className="text-3xl cursor-pointer" />
-                </div>
-                <div>
-                  <FaTwitterSquare className="text-3xl cursor-pointer" />
-                </div>
-                <div>
-                  <FaSquareInstagram className="text-3xl cursor-pointer" />
-                </div>
+                <Link to="/facebook">
+                  <div>
+                    <FaSquareFacebook className="text-3xl cursor-pointer" />
+                  </div>
+                </Link>
+
+                <Link to="/twitter">
+                  <div>
+                    <FaTwitterSquare className="text-3xl cursor-pointer" />
+                  </div>
+                </Link>
+
+                <Link to="/instagram">
+                  <div>
+                    <FaSquareInstagram className="text-3xl cursor-pointer" />
+                  </div>
+                </Link>
               </div>
             </div>
           </div>
@@ -66,13 +77,16 @@ export default function Navbar() {
 
         {/* <!-- ========== second  Section ========== --> */}
         <div className="flex whitespace-nowrap justify-between items-center py-10 ">
-          <div className=" w-15 h-15 sm:w-25 sm:h-25 ">
-            <img
-              className="w-full rounded-full cursor-pointer h-full object-cover bg-center"
-              src="https://plus.unsplash.com/premium_photo-1669652639337-c513cc42ead6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D"
-              alt=""
-            />
-          </div>
+          <Link to="/">
+            {" "}
+            <div className=" w-15 mr-3 h-15 sm:w-25 sm:h-25 ">
+              <img
+                className="w-full rounded-full cursor-pointer h-full object-cover bg-center"
+                src="https://plus.unsplash.com/premium_photo-1669652639337-c513cc42ead6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Ym9va3xlbnwwfHwwfHx8MA%3D%3D"
+                alt=""
+              />
+            </div>
+          </Link>
           <div className="text-4xl lg:hidden " onClick={showMobile}>
             <MdOutlineMenuBook />
           </div>
@@ -87,32 +101,56 @@ export default function Navbar() {
           )}
 
           {first && (
-            <div className="z-33 absolute top-0 min-h-screen right-0 w-full lg:hidden sm:w-[300px] px-4 py-10 bg-color1 ">
+            <div className="z-33   absolute top-0 min-h-screen right-0 w-full lg:hidden sm:w-[300px] px-4 py-10 bg-color1  ">
               <div className="flex flex-col  items-center">
                 <ul className="flex flex-col items-center  text-lg font-semibold gap-4 mr-5">
-                  <li className="cursor-pointer ">Home</li>
-                  <li className="cursor-pointer ">Universites</li>
-                  <li className="cursor-pointer ">Admission</li>
-                  <li className="cursor-pointer ">e-Subagent</li>
-                  <li className="cursor-pointer ">North Cyprus</li>
-                  <li className="cursor-pointer ">Contact</li>
-                  <li className="cursor-pointer ">Login</li>
+                  <Link to="/">
+                    <li className="cursor-pointer">Home</li>
+                  </Link>
+                  <Link to="/universities">
+                    <li className="cursor-pointer">Universites</li>
+                  </Link>
+                  <Link to="/admission">
+                    <li className="cursor-pointer">Admission</li>
+                  </Link>
+                  <Link to="/e-subagent">
+                    <li className="cursor-pointer">e-Subagent</li>
+                  </Link>
+                  <Link to="/north-cyprus">
+                    <li className="cursor-pointer">North Cyprus</li>
+                  </Link>
+                  <Link to="/contact">
+                    <li className="cursor-pointer">Contact</li>
+                  </Link>
+                  <Link to="/login">
+                    <li className="cursor-pointer">Login</li>
+                  </Link>
                 </ul>
-                <button className="px-3 py-2 text-lg bg-color1 cursor-pointer rounded-sm font-semibold">
-                  Apply Now
-                </button>
+                <Link to="/contact">
+                  <button className="px-3  py-2 text-lg bg-color1 cursor-pointer rounded-sm font-semibold">
+                    Apply Now
+                  </button>
+                </Link>
               </div>
 
               <div className=" flex justify-center mt-5  gap-5  text-center">
-                <div>
-                  <FaSquareFacebook className="text-3xl cursor-pointer" />
-                </div>
-                <div>
-                  <FaTwitterSquare className="text-3xl cursor-pointer" />
-                </div>
-                <div>
-                  <FaSquareInstagram className="text-3xl cursor-pointer" />
-                </div>
+                <Link to="/facebook">
+                  <div>
+                    <FaSquareFacebook className="text-3xl cursor-pointer" />
+                  </div>
+                </Link>
+
+                <Link to="/twitter">
+                  <div>
+                    <FaTwitterSquare className="text-3xl cursor-pointer" />
+                  </div>
+                </Link>
+
+                <Link to="/instagram">
+                  <div>
+                    <FaSquareInstagram className="text-3xl cursor-pointer" />
+                  </div>
+                </Link>
               </div>
             </div>
           )}
@@ -120,19 +158,33 @@ export default function Navbar() {
           {/* <!-- =========Mobile Side Bar Section ========== --> */}
 
           <div className="hidden lg:block">
-            <div className="flex   items-center">
+            <div className="flex  mr-4  items-center">
               <ul className="flex text-sm xl:text-lg font-semibold gap-4 mr-5">
-                <li className="cursor-pointer">Home</li>
-                <li className="cursor-pointer">Universites</li>
-                <li className="cursor-pointer">Admission</li>
-                <li className="cursor-pointer">e-Subagent</li>
-                <li className="cursor-pointer">North Cyprus</li>
-                <li className="cursor-pointer">Contact</li>
-                <li className="cursor-pointer">Login</li>
+                <Link to="/">
+                  <li className="cursor-pointer">Home</li>
+                </Link>
+                <Link to="/universities">
+                  <li className="cursor-pointer">Universites</li>
+                </Link>
+                <Link to="/admission">
+                  <li className="cursor-pointer">Admission</li>
+                </Link>
+                <Link to="/e-subagent">
+                  <li className="cursor-pointer">e-Subagent</li>
+                </Link>
+                <Link to="/north-cyprus">
+                  <li className="cursor-pointer">North Cyprus</li>
+                </Link>
+                <Link to="/contact">
+                  <li className="cursor-pointer">Contact</li>
+                </Link>
+                <Link to="/login">
+                  <li className="cursor-pointer">Login</li>
+                </Link>
               </ul>
-              <button className="px-3 py-2 btnbg text-lg bg-color1 cursor-pointer rounded-sm font-semibold">
+             <Link to="/contact"> <button className="px-3 py-2 btnbg text-lg bg-color1 cursor-pointer rounded-sm font-semibold">
                 Apply Now
-              </button>
+              </button></Link>
             </div>
           </div>
           <div className="relative z-20 text-sm hidden lg:block cursor-pointer font-semibold group">
